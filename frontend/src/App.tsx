44de3204c3ac6ff77db4fe8af4c9a0f4b3e75433
Emmanuel <oluwatopealofe@gmail.com>
2024-09-19 13:32:26 -0400

    Setup react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-function App() {
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
+const App = () => {
   return (
-    <>
-      <div>
-      </div>
-    </>
+    <Routes>
+      <Route path='/' element={<Home /> } />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="p-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-base">Hello, world!</h1>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
