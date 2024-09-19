f077a9e7641fa46594ff2644f6a16c4dd41dd965
Emmanuel <oluwatopealofe@gmail.com>
2024-09-19 18:44:19 -0400

    Create register user resource

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,3 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models import db, User
+from config import ApplicationConfig
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+app.config.from_object(ApplicationConfig)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+bcrypt = Bcrypt(app) 
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user_exists = User.query.filter_by(email=email).first() is not None
+
+    if user_exists:
+        return jsonify({"error": "User already exists"}), 409
+    
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') 
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
