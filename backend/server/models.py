0995246574e82ec1ba6e9724505a4f70f780dd42
Emmanuel <oluwatopealofe@gmail.com>
2024-09-19 16:20:14 -0400

    Create user model

---
+++ backend/server/models.py
@@ -0,0 +1,17 @@
+"""Module describing models to be used within flask app."""
+
+from uuid import uuid4
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def get_uuid():
+    return uuid4().hex
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
+    email = db.Column(db.String(345), unique=True)
+    password = db.Column(db.Text, nullable=False)
+
+
