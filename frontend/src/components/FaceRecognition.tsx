3b29dd8f8fce25c1150f3fd30f64bfc245f7fba9
Emmanuel <oluwatopealofe@gmail.com>
2024-09-22 21:19:59 -0400

    Add error handling

--- frontend/src/components/FaceRecognition.tsx
+++ frontend/src/components/FaceRecognition.tsx
@@ -12,6 +12,7 @@ type FaceRecognitionProps = {
 const FaceRecognition = ({ selectedFace, selectedPhotos }: FaceRecognitionProps) => {
     const [submitFaceRecognition, setSubmitFaceRecognition] = useState<boolean>(false)
     const [base64Images, setBase64Images] = useState<Base64Image[]>([]);
+    const [error, setError] = useState<boolean>(false)
 
     useEffect(() => {
         handleFaceRecognition();
@@ -58,6 +59,7 @@ const FaceRecognition = ({ selectedFace, selectedPhotos }: FaceRecognitionProps)
             })
             .catch((err) => {
                 console.log(err);
+                setError(true)
             });
     };
 
@@ -98,6 +100,14 @@ const FaceRecognition = ({ selectedFace, selectedPhotos }: FaceRecognitionProps)
 
                 )
             }
+            {error ? (
+                <div className="mt-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong className="font-bold"></strong>
+                    <span className="block sm:inline">Something went wrong. Please try again.</span>
+                </div>
+            ) : (
+                <></>
+            )}
         </>
 
     )
